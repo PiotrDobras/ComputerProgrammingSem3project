751f4c93a79c110a48beb7a3e81cc5e9323330d2
PiotrDobras <piotr.jacek.dobras@gmail.com>
2019-01-30 19:46:19 +0100

    Map Generation Clear Addendum
    Previous commit accidentally made from private account. Just want to
    make that clear.

--- ComputerProgrammingSem3project/GameObject.cpp
+++ ComputerProgrammingSem3project/GameObject.cpp
@@ -2,7 +2,7 @@
 #include "ConsoleGraphics.h"
 
 void GameObject::DrawSelf(int cx, int cy) {
-	if (/*seen == true && */ cx >= 0 && cx < 80 && cy >= 0 && cy < 25) {
+	if (seen == true && cx >= 0 && cx < 80 && cy >= 0 && cy < 25) {
 		DrawAt(cx, cy, glyph, glyph_color);
 	}
 }
